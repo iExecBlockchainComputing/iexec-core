fbaccb538b1d4545d18b533ddd6d53479ba86063
Ugo Plouviez <up@iex.ec>
2018-09-24 17:20:05 +0200

    Add spring files

---
+++ src/main/java/com/iexec/core/IexecWorkerNewApplication.java
@@ -0,0 +1,12 @@
+package com.iexec.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IexecWorkerNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IexecWorkerNewApplication.class, args);
+	}
+}
