95c9618da613e42c2bed24f048ef5219cb059e8f
Ugo Plouviez <up@iex.ec>
2018-09-25 15:39:20 +0200

    Add dummy controller and Swagger

---
+++ src/main/java/com/iexec/core/DummyController.java
@@ -0,0 +1,22 @@
+package com.iexec.core;
+
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DummyController {
+
+    private static final String TEMPLATE = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name));
+    }
+}
