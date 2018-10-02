d6789998549689a20f600eaf0cdcc43f34febcd4
Ugo Plouviez <up@iex.ec>
2018-10-02 10:18:27 +0200

    Add unit tests for WorkerService

---
+++ src/test/java/com/iexec/core/worker/WorkerServiceTests.java
@@ -0,0 +1,105 @@
+package com.iexec.core.worker;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+public class WorkerServiceTests {
+
+    @Mock
+    private WorkerRepository workerRepository;
+
+    @InjectMocks
+    private WorkerService workerService;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void shouldNotAddNewWorker() {
+        String workerName = "worker1";
+        Worker existingWorker = Worker.builder()
+                .id("1")
+                .name(workerName)
+                .lastAliveDate(new Date())
+                .build();
+
+        when(workerRepository.findByName(workerName)).thenReturn(Optional.of(existingWorker));
+        Worker addedWorker = workerService.addWorker(workerName);
+        assertThat(addedWorker).isEqualTo(existingWorker);
+    }
+
+    @Test
+    public void shouldAddNewWorker() {
+        String workerName = "worker1";
+        Worker worker = Worker.builder()
+                .id("1")
+                .name(workerName)
+                .lastAliveDate(new Date())
+                .build();
+        when(workerRepository.findByName(workerName)).thenReturn(Optional.empty());
+        when(workerRepository.save(Mockito.any())).thenReturn(worker);
+
+        Worker addedWorker = workerService.addWorker(workerName);
+        // check that the save method was called once
+        Mockito.verify(workerRepository, Mockito.times(1)).save(Mockito.any());
+        assertThat(addedWorker).isEqualTo(worker);
+    }
+
+    @Test
+    public void shouldUpdateLastAlive() throws ParseException {
+        // init
+        String workerName = "worker1";
+        Date oldLastAlive = new SimpleDateFormat("yyyy-MM-dd").parse("2018-01-01");
+        Worker worker = Worker.builder()
+                .id("1")
+                .name(workerName)
+                .lastAliveDate(oldLastAlive)
+                .build();
+        when(workerRepository.findByName(workerName)).thenReturn(Optional.of(worker));
+
+        // call
+        Optional<Worker> updatedWorker = workerService.updateLastAlive(workerName);
+
+        // check argument passed to the save method
+        ArgumentCaptor<Worker> argument = ArgumentCaptor.forClass(Worker.class);
+        Mockito.verify(workerRepository).save(argument.capture());
+        assertThat(argument.getValue().getId()).isEqualTo(worker.getId());
+        assertThat(argument.getValue().getName()).isEqualTo(worker.getName());
+        // check that the save method was called with a lastAlive parameter updated less than a second ago
+        Date now = new Date();
+        long duration = now.getTime() - argument.getValue().getLastAliveDate().getTime();
+        long diffInSeconds = TimeUnit.MILLISECONDS.toSeconds(duration);
+        assertThat(diffInSeconds).isEqualTo(0);
+
+        // check object returned by the method
+        assertThat(updatedWorker.isPresent()).isTrue();
+        assertThat(updatedWorker.get().getId()).isEqualTo(worker.getId());
+        assertThat(updatedWorker.get().getName()).isEqualTo(worker.getName());
+        duration = now.getTime() - updatedWorker.get().getLastAliveDate().getTime();
+        diffInSeconds = TimeUnit.MILLISECONDS.toSeconds(duration);
+        assertThat(diffInSeconds).isEqualTo(0);
+    }
+
+
+    @Test
+    public void shouldNotFindWorkerForUpdateLastAlive() {
+        String workerName = "worker1";
+        when(workerRepository.findByName(workerName)).thenReturn(Optional.empty());
+
+        Optional<Worker> optional = workerService.updateLastAlive(workerName);
+        assertThat(optional.isPresent()).isFalse();
+    }
+
+}
