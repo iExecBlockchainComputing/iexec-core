59031f8fc032585ca22b5488f86997831be2efcc
Jérémy James Toussaint <jeremy.toussaint@iex.ec>
2018-10-10 11:17:51 +0200

    Give work if worker in database

--- src/main/java/com/iexec/core/task/TaskService.java
+++ src/main/java/com/iexec/core/task/TaskService.java
@@ -92,21 +92,21 @@ public class TaskService {
 
         int workerRunningReplicateNb = getRunningReplicatesOfWorker(workerName).size();
         Optional<Worker> worker = workerService.getWorker(workerName);
-        if (worker.isPresent()){
+        if (worker.isPresent()) {
             int workerCpuNb = worker.get().getCpuNb();
             if (workerRunningReplicateNb >= workerCpuNb) {
                 log.info("Worker asking for too many replicates [workerName: {}, workerRunningReplicateNb:{}, workerCpuNb:{}]",
                         workerName, workerRunningReplicateNb, workerCpuNb);
                 return Optional.empty();// return empty if worker has enough running tasks
             }
-        }
 
-        for (Task task : tasks) {
-            if (!task.hasWorkerAlreadyContributed(workerName) &&
-                    task.needMoreReplicates()) {
-                task.createNewReplicate(workerName);
-                taskRepository.save(task);
-                return task.getReplicate(workerName);
+            for (Task task : tasks) {
+                if (!task.hasWorkerAlreadyContributed(workerName) &&
+                        task.needMoreReplicates()) {
+                    task.createNewReplicate(workerName);
+                    taskRepository.save(task);
+                    return task.getReplicate(workerName);
+                }
             }
         }
 
