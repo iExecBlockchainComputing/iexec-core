af602648c1fe2c2f8b44cd7de0e2b55bd3055921
Ugo Plouviez <up@iex.ec>
2018-10-10 15:01:41 +0200

    Add synchronization in the new replicate creation process

--- src/main/java/com/iexec/core/Application.java
+++ src/main/java/com/iexec/core/Application.java
@@ -3,8 +3,11 @@ package com.iexec.core;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
+
 
 @SpringBootApplication
+@EnableRetry
 public class Application implements CommandLineRunner {
 
     public static void main(String[] args) {
