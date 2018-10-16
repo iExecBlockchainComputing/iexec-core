ab0b77b930ad40415166a59146160037aa30d9b8
Ugo Plouviez <up@iex.ec>
2018-10-16 10:52:58 +0200

    Add query for multiple status

--- src/main/java/com/iexec/core/task/TaskRepository.java
+++ src/main/java/com/iexec/core/task/TaskRepository.java
@@ -1,6 +1,7 @@
 package com.iexec.core.task;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,5 +11,8 @@ interface TaskRepository extends MongoRepository<Task, String> {
     Optional<Task> findById(String id);
 
     List<Task> findByCurrentStatus(TaskStatus status);
+
+    @Query("{ 'currentStatus': {$in: ?0} }")
+    List<Task> findByCurrentStatus(List<TaskStatus> status);
 
 }
