37781d50fdc45433a79604aa311233246b0614bf
Ugo Plouviez <up@iex.ec>
2019-01-16 17:30:05 +0100

    Add heartbeat in the configuration

--- src/main/java/com/iexec/core/pubsub/WebSocketConfig.java
+++ src/main/java/com/iexec/core/pubsub/WebSocketConfig.java
@@ -3,7 +3,6 @@ package com.iexec.core.pubsub;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -15,7 +14,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
+        config.enableSimpleBroker("/topic")
+                .setHeartbeatValue(new long[] {10000, 20000});
     }
 
     @Override
