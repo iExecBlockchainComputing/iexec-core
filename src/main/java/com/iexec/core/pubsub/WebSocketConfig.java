810af5feb5e0e3f6e2b580a170d31a2de77d8eb6
Ugo Plouviez <up@iex.ec>
2019-01-16 10:28:49 +0100

    Add websocket timer

--- src/main/java/com/iexec/core/pubsub/WebSocketConfig.java
+++ src/main/java/com/iexec/core/pubsub/WebSocketConfig.java
@@ -1,11 +1,13 @@
 package com.iexec.core.pubsub;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -22,5 +24,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/connect");
     }
 
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxTextMessageBufferSize(8192);
+        container.setMaxBinaryMessageBufferSize(8192);
+        container.setMaxSessionIdleTimeout(10 * 60 * 1000L); // 10 min
+        return container;
+    }
 
 }
