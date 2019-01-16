9208139fe64d793a9a7076a1b368f705344a63d3
Ugo Plouviez <up@iex.ec>
2019-01-16 17:39:29 +0100

    Add heartbeat in the configuration

--- src/main/java/com/iexec/core/pubsub/WebSocketConfig.java
+++ src/main/java/com/iexec/core/pubsub/WebSocketConfig.java
@@ -1,8 +1,10 @@
 package com.iexec.core.pubsub;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -12,10 +14,18 @@ import org.springframework.web.socket.server.standard.ServletServerContainerFact
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    private TaskScheduler messageBrokerTaskScheduler;
+
+    @Autowired
+    public void setMessageBrokerTaskScheduler(TaskScheduler taskScheduler) {
+        this.messageBrokerTaskScheduler = taskScheduler;
+    }
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic")
-                .setHeartbeatValue(new long[] {10000, 20000});
+                .setHeartbeatValue(new long[]{10000, 20000})
+                .setTaskScheduler(this.messageBrokerTaskScheduler);
     }
 
     @Override
