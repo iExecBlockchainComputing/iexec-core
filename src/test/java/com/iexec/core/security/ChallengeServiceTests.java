7b6249ffdf19b2fda98c45f227a95ec9a5b9cbe6
Zied Guesmi <Guesmy.Zied@gmail.com>
2019-01-17 10:53:17 +0100

    add test case

--- src/test/java/com/iexec/core/security/ChallengeServiceTests.java
+++ src/test/java/com/iexec/core/security/ChallengeServiceTests.java
@@ -8,7 +8,8 @@ import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class ChallengeServiceTests {
 
-    private final static String WALLET_WORKER = "0x1a69b2eb604db8eba185df03ea4f5288dcbbd248";
+    private final static String WALLET_WORKER_1 = "0x1a69b2eb604db8eba185df03ea4f5288dcbbd248";
+    private final static String WALLET_WORKER_2 = "0x2a69b2eb604db8eba185df03ea4f5288dcbbd248";
 
     private ChallengeService challengeService = new ChallengeService();
 
@@ -17,8 +18,15 @@ public class ChallengeServiceTests {
 
     @Test
     public void shouldGetSameChallengeForSameWallet() {
-        String challenge1 = challengeService.getChallenge(WALLET_WORKER);
-        String challenge2 = challengeService.getChallenge(WALLET_WORKER);
+        String challenge1 = challengeService.getChallenge(WALLET_WORKER_1);
+        String challenge2 = challengeService.getChallenge(WALLET_WORKER_1);
         assertThat(challenge1).isEqualTo(challenge2);
+    }
+
+    @Test
+    public void shouldGetDifferentChallengesForDifferentWallets() {
+        String challenge1 = challengeService.getChallenge(WALLET_WORKER_1);
+        String challenge2 = challengeService.getChallenge(WALLET_WORKER_2);
+        assertThat(challenge1).isNotEqualTo(challenge2);
     }
 }
