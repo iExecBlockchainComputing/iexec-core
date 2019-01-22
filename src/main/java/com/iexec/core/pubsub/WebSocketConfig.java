49381e66472e708124795078a1b0d27ac3abd373
Ugo Plouviez <up@iex.ec>
2019-01-22 14:24:51 +0100

    Add sockJS

--- src/main/java/com/iexec/core/pubsub/WebSocketConfig.java
+++ src/main/java/com/iexec/core/pubsub/WebSocketConfig.java
@@ -30,6 +30,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/connect");
+        registry.addEndpoint("/connect").withSockJS();
     }
 }
