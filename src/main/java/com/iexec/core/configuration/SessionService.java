86908ee78051ae290b70d84faca1a36b584bdeef
Ugo Plouviez <up@iex.ec>
2019-01-28 15:10:06 +0100

    Minor change

--- src/main/java/com/iexec/core/configuration/SessionService.java
+++ src/main/java/com/iexec/core/configuration/SessionService.java
@@ -1,26 +1,16 @@
 package com.iexec.core.configuration;
 
 import org.apache.commons.lang3.RandomStringUtils;
-import org.springframework.stereotype.Service;
-
-import javax.annotation.PostConstruct;
 
 /**
  * This simple service will generate a random session id when the scheduler is started, it will be send to workers when
  * they ping the scheduler. If they see that the session id has changed, it means that the scheduler has restarted.
  */
-@Service
 public class SessionService {
 
-    private String sessionId;
+    private static final String sessionId = RandomStringUtils.randomAlphanumeric(10);
 
-    @PostConstruct
-    private void init(){
-        sessionId = RandomStringUtils.randomAlphanumeric(10);
-    }
-
-    public String getSessionId(){
+    public static String getSessionId(){
         return sessionId;
     }
-
 }
