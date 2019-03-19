1a0ed0ad232f1d3d07b2eb68848c9f19940f9513
Jérémy James Toussaint <jeremy.toussaint@iex.ec>
2019-03-19 12:03:18 +0100

    ComputeFailed to Contribute

--- src/main/java/com/iexec/core/workflow/ReplicateWorkflow.java
+++ src/main/java/com/iexec/core/workflow/ReplicateWorkflow.java
@@ -2,12 +2,33 @@ package com.iexec.core.workflow;
 
 import com.iexec.common.replicate.ReplicateStatus;
 
+import java.util.List;
+
 import static com.iexec.common.replicate.ReplicateStatus.*;
 
 
 public class ReplicateWorkflow extends Workflow<ReplicateStatus> {
 
     private static ReplicateWorkflow instance;
+    private final List<ReplicateStatus> APP_DOWNLOADED_AND_APP_DOWNLOAD_FAILED_LIST = toList(
+            APP_DOWNLOADED,
+            APP_DOWNLOAD_FAILED
+    );
+    private final List<ReplicateStatus> DATA_DOWNLOADED_AND_DATA_DOWNLOAD_FAILED_LIST = toList(
+            DATA_DOWNLOADED,
+            DATA_DOWNLOAD_FAILED
+    );
+    private final List<ReplicateStatus> COMPUTED_AND_COMPUTE_FAILED_LIST = toList(
+            COMPUTED,
+            COMPUTE_FAILED
+    );
+    private final List<ReplicateStatus> CAN_AND_CANT_CONTRIBUTE_LIST = toList(
+            CANT_CONTRIBUTE_SINCE_STAKE_TOO_LOW,
+            CANT_CONTRIBUTE_SINCE_TASK_NOT_ACTIVE,
+            CANT_CONTRIBUTE_SINCE_AFTER_DEADLINE,
+            CANT_CONTRIBUTE_SINCE_CONTRIBUTION_ALREADY_SET,
+            CAN_CONTRIBUTE
+    );
 
     private ReplicateWorkflow() {
         super();
@@ -17,40 +38,20 @@ public class ReplicateWorkflow extends Workflow<ReplicateStatus> {
         addTransition(RUNNING, APP_DOWNLOADING);
 
         // app
-        addTransition(APP_DOWNLOADING, toList(APP_DOWNLOADED, APP_DOWNLOAD_FAILED));
-
-        addTransition(APP_DOWNLOAD_FAILED, toList(
-                // DATA_DOWNLOADING,
-                CANT_CONTRIBUTE_SINCE_STAKE_TOO_LOW,
-                CANT_CONTRIBUTE_SINCE_TASK_NOT_ACTIVE,
-                CANT_CONTRIBUTE_SINCE_AFTER_DEADLINE,
-                CANT_CONTRIBUTE_SINCE_CONTRIBUTION_ALREADY_SET,
-                CAN_CONTRIBUTE));
-
+        addTransition(APP_DOWNLOADING, APP_DOWNLOADED_AND_APP_DOWNLOAD_FAILED_LIST);
+        addTransition(APP_DOWNLOAD_FAILED, CAN_AND_CANT_CONTRIBUTE_LIST);
         addTransition(APP_DOWNLOADED, DATA_DOWNLOADING);
 
         // data
-        addTransition(DATA_DOWNLOADING, toList(DATA_DOWNLOADED, DATA_DOWNLOAD_FAILED));
-
-        addTransition(DATA_DOWNLOAD_FAILED, toList(
-                // COMPUTING,
-                CANT_CONTRIBUTE_SINCE_STAKE_TOO_LOW,
-                CANT_CONTRIBUTE_SINCE_TASK_NOT_ACTIVE,
-                CANT_CONTRIBUTE_SINCE_AFTER_DEADLINE,
-                CANT_CONTRIBUTE_SINCE_CONTRIBUTION_ALREADY_SET,
-                CAN_CONTRIBUTE));
-
+        addTransition(DATA_DOWNLOADING, DATA_DOWNLOADED_AND_DATA_DOWNLOAD_FAILED_LIST);
+        addTransition(DATA_DOWNLOAD_FAILED, CAN_AND_CANT_CONTRIBUTE_LIST);
         addTransition(DATA_DOWNLOADED, COMPUTING);
 
         // computation
-        addTransition(COMPUTING, toList(COMPUTED, COMPUTE_FAILED));
-
-        addTransition(COMPUTED, toList(
-                CANT_CONTRIBUTE_SINCE_STAKE_TOO_LOW,
-                CANT_CONTRIBUTE_SINCE_TASK_NOT_ACTIVE,
-                CANT_CONTRIBUTE_SINCE_AFTER_DEADLINE,
-                CANT_CONTRIBUTE_SINCE_CONTRIBUTION_ALREADY_SET,
-                CAN_CONTRIBUTE));
+        addTransition(COMPUTING, COMPUTED_AND_COMPUTE_FAILED_LIST);
+        for (ReplicateStatus from : COMPUTED_AND_COMPUTE_FAILED_LIST) {
+            addTransition(from, CAN_AND_CANT_CONTRIBUTE_LIST);
+        }
 
         // contribution
         addTransition(CAN_CONTRIBUTE, toList(CONTRIBUTING, OUT_OF_GAS));
