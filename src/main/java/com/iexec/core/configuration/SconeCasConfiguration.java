d824997e74ded31fa7d1e769d039734f8dd7377a
Zied <guesmy.zied@gmail.com>
2019-06-25 10:09:00 +0100

    Add CAS description

--- src/main/java/com/iexec/core/configuration/SconeCasConfiguration.java
+++ src/main/java/com/iexec/core/configuration/SconeCasConfiguration.java
@@ -7,6 +7,16 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 
+/**
+ * CAS: Configuration and Attestation Service.
+ * It handles configurations and secret provisioning: a user uploads secrets
+ * and configuration infos for a specific service to the CAS.
+ * When a service wants to access those secrets, it sends a report with its MREnclave.
+ * If the MREnclave is as expected, CAS will return the data (in a secure way).
+ * 
+ * MREnclave: an enclave identifier, created by hashing all its
+ * code. It guarantees that a code behaves exactly as expected.
+ */
 @Component
 @Getter
 @NoArgsConstructor
