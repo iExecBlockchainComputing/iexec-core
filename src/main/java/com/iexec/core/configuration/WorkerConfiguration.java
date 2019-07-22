5d5d1aaeb5e1386f48a2629f9c3d22944e5e0e2b
Ugo Plouviez <up@iex.ec>
2019-07-22 14:49:59 +0200

    Add whitelist to the properties file

--- src/main/java/com/iexec/core/configuration/WorkerConfiguration.java
+++ src/main/java/com/iexec/core/configuration/WorkerConfiguration.java
@@ -4,7 +4,9 @@ import lombok.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@Getter
+import java.util.Arrays;
+import java.util.List;
+
 @Setter
 @Component
 public class WorkerConfiguration {
@@ -15,4 +17,19 @@ public class WorkerConfiguration {
     @Value("${workers.requiredWorkerVersion}")
     private String requiredWorkerVersion;
 
+    @Value("${workers.whitelist}")
+    private String[] whitelist;
+
+    // getters are overridden since the whitelist should return a list, not an array
+    public long getAskForReplicatePeriod() {
+        return askForReplicatePeriod;
+    }
+
+    public String getRequiredWorkerVersion() {
+        return requiredWorkerVersion;
+    }
+
+    public List<String> getWhitelist() {
+        return Arrays.asList(whitelist);
+    }
 }
