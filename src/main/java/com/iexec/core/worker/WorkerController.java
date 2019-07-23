82cafa34bc5e78b84ae6b8009417fccfd4251301
Ugo Plouviez <up@iex.ec>
2019-07-23 12:07:07 +0200

    Changes following PR

--- src/main/java/com/iexec/core/worker/WorkerController.java
+++ src/main/java/com/iexec/core/worker/WorkerController.java
@@ -8,11 +8,7 @@ import com.iexec.common.utils.BytesUtils;
 import com.iexec.common.utils.SignatureUtils;
 import com.iexec.core.chain.ChainConfig;
 import com.iexec.core.chain.CredentialsService;
-import com.iexec.core.configuration.ResultRepositoryConfiguration;
-import com.iexec.core.configuration.SconeCasConfiguration;
-import com.iexec.core.configuration.SessionService;
-import com.iexec.core.configuration.SmsConfiguration;
-import com.iexec.core.configuration.WorkerConfiguration;
+import com.iexec.core.configuration.*;
 import com.iexec.core.security.ChallengeService;
 import com.iexec.core.security.JwtTokenProvider;
 import lombok.extern.slf4j.Slf4j;
@@ -21,7 +17,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.web3j.crypto.Hash;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 import static org.springframework.http.ResponseEntity.ok;
 import static org.springframework.http.ResponseEntity.status;
@@ -85,6 +84,10 @@ public class WorkerController {
     public ResponseEntity getToken(@RequestParam(name = "walletAddress") String walletAddress,
                                    @RequestBody Signature signature) {
 
+        if (!workerService.isAllowedToJoin(walletAddress)) {
+            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+        }
+
         String challenge = challengeService.getChallenge(walletAddress);
         byte[] hashTocheck = Hash.sha3(BytesUtils.stringToBytes(challenge));
 
@@ -101,7 +104,8 @@ public class WorkerController {
     public ResponseEntity registerWorker(@RequestHeader("Authorization") String bearerToken,
                                          @RequestBody WorkerConfigurationModel model) {
         String workerWalletAddress = jwtTokenProvider.getWalletAddressFromBearerToken(bearerToken);
-        if (workerWalletAddress.isEmpty() || !workerService.isAllowedToJoin(workerWalletAddress)) {
+
+        if (workerWalletAddress.isEmpty()){
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED.value()).build();
         }
 
