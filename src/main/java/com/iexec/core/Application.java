d772aecf6bd07c764478a462c74fdface6113323
Jérémy James Toussaint <jeremy.toussaint@iex.ec>
2019-09-19 17:30:04 +0200

    Useless import

--- src/main/java/com/iexec/core/Application.java
+++ src/main/java/com/iexec/core/Application.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.elasticsearch.jest.JestAutoConfiguration;
 
 
 @SpringBootApplication
