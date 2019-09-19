2d2964697c6a6452ca458faf0714b52fafc3d22e
Jérémy James Toussaint <jeremy.toussaint@iex.ec>
2019-09-19 14:15:14 +0200

    Graylog init

--- src/main/java/com/iexec/core/Application.java
+++ src/main/java/com/iexec/core/Application.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.elasticsearch.jest.JestAutoConfiguration;
 
 
 @SpringBootApplication
