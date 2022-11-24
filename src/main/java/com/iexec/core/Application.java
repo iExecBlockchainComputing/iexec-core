a4768b2e6341a89ed4f317f3c050bf1991635789
Maxence Cornaton <maxence.cornaton@iex.ec>
2022-11-24 15:54:04 +0100

    Add `@ConfigurationPropertiesScan`

--- src/main/java/com/iexec/core/Application.java
+++ src/main/java/com/iexec/core/Application.java
@@ -19,10 +19,12 @@ package com.iexec.core;
 import io.changock.runner.spring.v5.config.EnableChangock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 
 @EnableChangock
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class Application {
 
     public static void main(String[] args) {
