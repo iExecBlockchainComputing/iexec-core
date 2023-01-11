42cffa74981d938e38d3c9a782b0a537c2ec53f4
Maxence Cornaton <maxence.cornaton@iex.ec>
2023-01-11 11:17:38 +0100

    Add `shouldAddTaskASingleTime` test

---
+++ src/test/java/com/iexec/core/task/TaskServiceRealRepositoryTest.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2023 IEXEC BLOCKCHAIN TECH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.iexec.core.task;
+
+import com.iexec.core.chain.IexecHubService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.*;
+
+import static com.iexec.core.task.TaskTestsUtils.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+@DataMongoTest
+@Testcontainers
+class TaskServiceRealRepositoryTest {
+    private final long maxExecutionTime = 60000;
+    private final Date contributionDeadline = new Date();
+    private final Date finalDeadline = new Date();
+
+    @Container
+    private static final MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:4.2"));
+
+    @DynamicPropertySource
+    static void registerProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.host", mongoDBContainer::getContainerIpAddress);
+        registry.add("spring.data.mongodb.port", () -> mongoDBContainer.getMappedPort(27017));
+    }
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Mock
+    private IexecHubService iexecHubService;
+
+    private TaskService taskService;
+
+    @BeforeEach
+    void init() {
+        MockitoAnnotations.openMocks(this);
+        taskService = new TaskService(taskRepository, iexecHubService);
+    }
+
+    @Test
+    void shouldAddTaskASingleTime() {
+        final Task task = getStubTask(maxExecutionTime);
+        task.changeStatus(TaskStatus.INITIALIZED);
+
+        final ExecutorService executorService = Executors.newFixedThreadPool(2);
+        final List<Future<Optional<Task>>> executions = new ArrayList<>(2);
+        for (int i = 0; i < 2; i++) {
+            executions.add(executorService.submit(() -> taskService.addTask(CHAIN_DEAL_ID, 0, 0, DAPP_NAME, COMMAND_LINE,
+                    2, maxExecutionTime, "0x0", contributionDeadline, finalDeadline)));
+        }
+
+        executions.forEach(execution -> {
+            try {
+                execution.get(100, TimeUnit.MILLISECONDS);
+            } catch (ExecutionException e) {
+                if (e.getCause() instanceof DuplicateKeyException) {
+                    fail("Task has been added twice. Should not happen!");
+                }
+            } catch (InterruptedException | TimeoutException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        assertThat(taskRepository.findByChainTaskId(CHAIN_TASK_ID)).isPresent();
+    }
+}
