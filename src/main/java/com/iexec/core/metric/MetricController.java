7a1899ac301b12214f5036ab490107393923e576
Jeremy Bernard <jeremy.bernard@iex.ec>
2023-02-16 17:30:47 +0100

    Use @GetMapping in MetricController

--- src/main/java/com/iexec/core/metric/MetricController.java
+++ src/main/java/com/iexec/core/metric/MetricController.java
@@ -16,10 +16,8 @@
 
 package com.iexec.core.metric;
 
-
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.ResponseEntity.ok;
@@ -27,15 +25,13 @@ import static org.springframework.http.ResponseEntity.ok;
 @RestController
 public class MetricController {
 
-
-    private MetricService metricService;
+    private final MetricService metricService;
 
     public MetricController(MetricService metricService) {
         this.metricService = metricService;
     }
 
-
-    @RequestMapping(method = RequestMethod.GET, path = "/metrics")
+    @GetMapping("/metrics")
     public ResponseEntity<PlatformMetric> getPlatformMetric() {
         return ok(metricService.getPlatformMetrics());
     }
