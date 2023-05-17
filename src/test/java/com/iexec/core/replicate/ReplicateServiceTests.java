02f42513fb624c0ee2d34ac9b15d2c113b06d875
Maxence Cornaton <maxence.cornaton@iex.ec>
2023-05-17 09:19:44 +0200

    Fix `shouldAuthorizeUpdateOnContributeAndFinalizeDone` test

--- src/test/java/com/iexec/core/replicate/ReplicateServiceTests.java
+++ src/test/java/com/iexec/core/replicate/ReplicateServiceTests.java
@@ -1284,13 +1284,16 @@ class ReplicateServiceTests {
                 .modifier(WORKER)
                 .status(CONTRIBUTE_AND_FINALIZE_DONE)
                 .build();
-        final TaskDescription task = TaskDescription.builder().chainTaskId(CHAIN_TASK_ID).build();
+        final TaskDescription task = TaskDescription
+                .builder()
+                .chainTaskId(CHAIN_TASK_ID)
+                .isTeeTask(true)
+                .build();
 
         when(replicatesRepository.findByChainTaskId(CHAIN_TASK_ID)).thenReturn(Optional.of(replicatesList));
         when(iexecHubService.repeatIsRevealedTrue(CHAIN_TASK_ID, WALLET_WORKER_1))
                 .thenReturn(true);
-        when(iexecHubService.getTaskDescriptionFromChain(CHAIN_TASK_ID)).thenReturn(Optional.of(task));
-        when(resultService.isResultUploaded(CHAIN_TASK_ID)).thenReturn(true);
+        when(iexecHubService.getTaskDescription(CHAIN_TASK_ID)).thenReturn(task);
         when(iexecHubService.isTaskInCompletedStatusOnChain(CHAIN_TASK_ID)).thenReturn(true);
 
         assertThat(replicatesService.canUpdateReplicateStatus(CHAIN_TASK_ID, WALLET_WORKER_1, statusUpdate, null))
