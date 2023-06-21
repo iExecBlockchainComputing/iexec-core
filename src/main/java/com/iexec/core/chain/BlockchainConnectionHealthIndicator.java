ca91ce75900b79f2fd0d4704a4bea338712febe8
Maxence Cornaton <maxence.cornaton@iex.ec>
2023-06-21 14:33:41 +0200

    Add `BlockchainConnectionHealthIndicator`

---
+++ src/main/java/com/iexec/core/chain/BlockchainConnectionHealthIndicator.java
@@ -0,0 +1,87 @@
+package com.iexec.core.chain;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.time.Duration;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Component
+public class BlockchainConnectionHealthIndicator implements HealthIndicator {
+    private final int pollingIntervalInBlocks = 3;
+    private final int maxConsecutiveFailures = 4;
+    private final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+
+    private final Web3jService web3jService;
+    private final Duration pollingInterval;
+
+    private int consecutiveFailures = 0;
+    private boolean outOfService = false;
+
+    public BlockchainConnectionHealthIndicator(Web3jService web3jService,
+                                               ChainConfig chainConfig) {
+        this.web3jService = web3jService;
+        this.pollingInterval = chainConfig.getBlockTime().multipliedBy(pollingIntervalInBlocks);
+    }
+
+    @PostConstruct
+    void scheduleMonitoring() {
+        executor.scheduleAtFixedRate(this::checkConnection, 0, pollingInterval.toSeconds(), TimeUnit.SECONDS);
+    }
+
+    /**
+     * Check blockchain is reachable by retrieving the latest block number.
+     * <p>
+     * If it isn't, then increment {@link BlockchainConnectionHealthIndicator#consecutiveFailures} counter.
+     * If counter reaches {@link BlockchainConnectionHealthIndicator#maxConsecutiveFailures},
+     * then this Health Indicator becomes {@link Status#OUT_OF_SERVICE}.
+     * <p>
+     * If blockchain is reachable, then reset the counter.
+     * <p>
+     * /!\ If the indicator becomes {@code OUT_OF_SERVICE}, then it stays as is until the Scheduler is restarted
+     * even if the blockchain is later available again.
+     */
+    void checkConnection() {
+        final long latestBlockNumber = web3jService.getLatestBlockNumber();
+        if (latestBlockNumber == 0) {
+            ++consecutiveFailures;
+            if (consecutiveFailures >= maxConsecutiveFailures) {
+                outOfService = true;
+                log.error("Blockchain hasn't been accessed for a long period. " +
+                        "This Scheduler is now OUT-OF-SERVICE until it is restarted." +
+                        "[unavailabilityPeriod:{}]", pollingInterval.multipliedBy(maxConsecutiveFailures));
+            } else {
+                log.warn("Blockchain is unavailable. Will retry connection." +
+                        "[unavailabilityPeriod:{}, nextRetry:{}]",
+                        pollingInterval.multipliedBy(consecutiveFailures), pollingInterval);
+            }
+        } else {
+            if (!outOfService) {
+                log.info("Blockchain connection is now restored after a period of unavailability." +
+                        "[unavailabilityPeriod:{}]", pollingInterval.multipliedBy(consecutiveFailures));
+            }
+            consecutiveFailures = 0;
+        }
+    }
+
+
+    @Override
+    public Health health() {
+        final Health.Builder healthBuilder = outOfService
+                ? Health.outOfService()
+                : Health.up();
+
+        return healthBuilder
+                .withDetail("pollingInterval", pollingInterval)
+                .withDetail("consecutiveFailures", consecutiveFailures)
+                .withDetail("maxConsecutiveFailuresBeforeOutOfService", maxConsecutiveFailures)
+                .build();
+    }
+}
