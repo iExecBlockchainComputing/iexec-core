7e0e6d55f549c96623e2d8ad8aaa66daa0728766
Frederic <frederic.cordier@iex.ec>
2024-04-23 13:29:11 +0200

    Move `PublicConfiguration` class from `iexec-common` to `iexec-core-library` (#699)

--- src/test/java/com/iexec/core/worker/WorkerControllerTests.java
+++ src/test/java/com/iexec/core/worker/WorkerControllerTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2023 IEXEC BLOCKCHAIN TECH
+ * Copyright 2020-2024 IEXEC BLOCKCHAIN TECH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,9 +16,9 @@
 
 package com.iexec.core.worker;
 
-import com.iexec.common.config.PublicConfiguration;
 import com.iexec.common.config.WorkerModel;
 import com.iexec.commons.poco.security.Signature;
+import com.iexec.core.config.PublicConfiguration;
 import com.iexec.core.configuration.PublicConfigurationService;
 import com.iexec.core.security.ChallengeService;
 import com.iexec.core.security.JwtTokenProvider;
@@ -182,7 +182,7 @@ class WorkerControllerTests {
     void shouldNotGetTokenSinceSignatureNotValid() {
         when(workerService.isAllowedToJoin(WALLET)).thenReturn(true);
         when(challengeService.getChallenge(WALLET)).thenReturn(CHALLENGE);
-        
+
         ResponseEntity<String> response = workerController.getToken(WALLET, INVALID_SIGNATURE);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
     }
