6be9fac9eab8f08bf6152d504d9e68f3a9a29d0c
Jeremy Bernard <jeremy.bernard@iex.ec>
2025-03-07 15:11:19 +0100

    Fix Spring Security deprecations after Spring Boot 3.3.8 upgrade (#740)

--- src/main/java/com/iexec/core/security/WebSecurityConfig.java
+++ src/main/java/com/iexec/core/security/WebSecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -30,13 +31,10 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // Disable CSRF (cross site request forgery)
-        http.csrf().disable();
-
+        http.csrf(AbstractHttpConfigurer::disable);
         // No session will be created or used by spring security
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-        http.authorizeHttpRequests()
-                .anyRequest().permitAll();
+        http.sessionManagement(sessionMgt -> sessionMgt.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.authorizeHttpRequests(request -> request.anyRequest().permitAll());
         return http.build();
     }
 }
